a68e3ac98e2df862efa21ff5f12221f04188bc6d
Maajid Nazrulla <gmakestyle@users.noreply.github.com>
2023-05-07 11:34:48 +0900

    Gmakestyle/scroll (#20)
    * Update pages so that BuySell is root
    
    * Change the modal header to just be the button because the text is redundant
    
    * Make some improvements to colors
    
    ---------
    
    Co-authored-by: gmakestyle <stylegmake@gmail.com>

--- components/HelixTransactionModal.tsx
+++ components/HelixTransactionModal.tsx
@@ -1,42 +1,87 @@
 import type { NextPage } from 'next'
 import { RootState } from '../store';
-import { useAppSelector } from  '../hooks';
+import { useAppSelector, useAppDispatch } from '../hooks';
+
+import { useTheme } from "@mui/material";
 import Box from "@mui/material/Box";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import Grid from "@mui/material/Grid";
 import CoinSelector from "./HelixTransactionCoinSelector";
 import InputForm from "./HelixTransactionInputForm";
+import { selectHelixFund } from "../features/wallet-data/walletDataSlice";
 
 const HelixTransactionModal: NextPage = () => {
     const { value } = useAppSelector((state: RootState) => state.buySellState);
 
-    const { etfs, selectedHelixFund } = useAppSelector((state: RootState) => state.walletCryptoData);
-    const selectedHelixFundData = etfs[selectedHelixFund];
+    const { etfs, selectedHelixFund } = useAppSelector(
+      (state: RootState) => state.walletCryptoData
+    );
+
+    const dispatch = useAppDispatch();
+
+    const handleChange = (event: SelectChangeEvent) => {
+      dispatch(selectHelixFund(event.target.value as string));
+    };
+    
+    const helixFundMenuItems = Object.entries(etfs).map(([ticker, etf]) => (
+      <MenuItem value={ticker} key={ticker}>
+        {value === "buy" ? "Buy" : "Sell"} {etf.name}
+      </MenuItem>
+    ));
+
+    const theme = useTheme();
 
     return (
-       <div>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <h1>{value === "buy" ? "Buy" : "Sell"} {selectedHelixFundData.name}</h1>
-      </Box>
-      <Grid container spacing={6} p={3}>
-        <Grid item xs={6}>
-          <CoinSelector />
+      <>
+        <Box
+          sx={{
+            maxHeight: "60px",
+            backgroundColor: value === "buy" ? "#3F51B5" : "#d32f2f",
+            marginBottom: "0.2rem",
+            "&:hover": {
+              background: value === "buy" ? "#1565c0" : "#ef5350",
+            },
+          }}
+        >
+          <FormControl fullWidth>
+            <Select
+              labelId="market-name"
+              id="market-name"
+              value={selectedHelixFund}
+              label="Market"
+              onChange={handleChange}
+              sx={{
+                color: "white",
+                fontWeight: "bold",
+                textAlign: "center",
+              }}
+            >
+              {helixFundMenuItems}
+            </Select>
+          </FormControl>
+        </Box>
+        <Grid container spacing={2} p={3}>
+          <Grid item xs={12}>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                mb: 2,
+              }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <CoinSelector />
+          </Grid>
+          <Grid item xs={6}>
+            <InputForm />
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <InputForm />
-        </Grid>
-      </Grid>
-    </div>
-    );    
-};
+      </>
+    );
+  };
     
 export default HelixTransactionModal;
-
-
-
---
+++ pages/Portfolio.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Head from 'next/head'
+import type { NextPage } from 'next'
+
+
+const Portfolio: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Helix</title>
+      </Head>
+    </>
+  )
+}
+
+export default Portfolio;
